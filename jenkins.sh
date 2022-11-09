f9dd43e40615655a6bd080f6d32f669a5dce332e
olaoluwani <88695505+olaoluwani@users.noreply.github.com>
2022-11-09 16:24:12 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -15,7 +15,9 @@ sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
 
 sudo yum -y install java-1.8.0-openjdk epel-release
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum -y install jenkins-2.277.4-1.1
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum -y install jenkins
 
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
